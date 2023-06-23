25777da5e6c8571eb02911eaf186118ce5b2894a
martin schenone <martinsche2010@gmail.com>
2023-06-22 21:44:17 -0300

    ultimo commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,32 @@
-<script>
-	import HeroHome from "../components/HeroHome.svelte";
-    export let data;
-</script>
 <section>
-    <h1>Welcome to AnimeX</h1>
-    <HeroHome {data}/>
-    <h3><a href="/anime">Click to see top animes and mangas</a></h3>
+	<h1>Welcome to AnimeX</h1>
+	<h3><a href="/anime">Click to see top animes and mangas</a></h3>
 </section>
 
 <style>
-    section{
-        width: 90%;
-        max-width: 1200px;
-        margin-inline: auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        min-height: 70vh;
-        color: #f1f1f1;
-    }
-    h3{
-        padding: 0.8rem;
-        transition: all 500ms;
-        border-radius: 0.5rem;
-        opacity: 0.5;
-    }
-    a{
-        text-decoration: none;
-        color: #f1f1f1;
-      
-    }
-    h3:hover{
-        background: #0a4a75;
-        opacity: 1;
-    }
+	section {
+		width: 90%;
+		max-width: 1200px;
+		margin-inline: auto;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		min-height: 70vh;
+		color: #f1f1f1;
+	}
+	h3 {
+		padding: 0.8rem;
+		transition: all 500ms;
+		border-radius: 0.5rem;
+		opacity: 0.5;
+	}
+	a {
+		text-decoration: none;
+		color: #f1f1f1;
+	}
+	h3:hover {
+		background: #0a4a75;
+		opacity: 1;
+	}
 </style>
